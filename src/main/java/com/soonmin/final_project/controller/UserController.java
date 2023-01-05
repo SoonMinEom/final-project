f22fd7ce5c7dff715eeda093729bfe9288d07008
soonmineom <skdlfma123@gmail.com>
2023-01-05 13:12:54 +0900

    UserController swagger 정리

--- src/main/java/com/soonmin/final_project/controller/UserController.java
+++ src/main/java/com/soonmin/final_project/controller/UserController.java
@@ -4,9 +4,11 @@ import com.soonmin.final_project.domain.Response;
 import com.soonmin.final_project.domain.dto.*;
 import com.soonmin.final_project.domain.dto.user.*;
 import com.soonmin.final_project.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -16,6 +18,7 @@ public class UserController {
 
     // 회원 가입
     @PostMapping("/join")
+    @ApiOperation(value = "회원 가입", notes = "회원 가입을 합니다.")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
         UserDto dto = userService.join(request);
         return Response.success(dto.toResponse());
@@ -23,6 +26,7 @@ public class UserController {
 
     // 로그인
     @PostMapping("/login")
+    @ApiOperation(value = "로그인", notes = "로그인을 합니다. JWT 토큰을 발행합니다.")
     public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
         String token = userService.login(request);
         return Response.success(new UserLoginResponse(token));
@@ -31,7 +35,8 @@ public class UserController {
     // ADMIN 승급기능, 정말로 승급만 가능
     // 초기 어드민 : userName = admin, password = string
     @PostMapping("/{id}/role/change")
-    public Response<RoleChangeResponse> roleChange(@PathVariable Integer id, Authentication authentication) {
+    @ApiOperation(value = "관리자 권한 부여", notes = "특정 유저에게 관리자 권한을 부여합니다. 이미 관리자 권한을 지닌 사용자만 권한 부여를 할 수 있습니다.")
+    public Response<RoleChangeResponse> roleChange(@PathVariable Integer id, @ApiIgnore Authentication authentication) {
         UserDto userDto = userService.roleChange(id, authentication.getName());
         return Response.success(new RoleChangeResponse(userDto.getUserName(), "ADMIN 승급 성공"));
     }
