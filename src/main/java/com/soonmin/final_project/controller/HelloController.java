59ef4546998ad0c8f18b4a38e0b913bb1b36bb22
soonmineom <skdlfma123@gmail.com>
2023-01-05 10:45:46 +0900

    swagger 에서 숨기기

--- src/main/java/com/soonmin/final_project/controller/HelloController.java
+++ src/main/java/com/soonmin/final_project/controller/HelloController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/api/v1/hello")
 @RequiredArgsConstructor
+@ApiIgnore
 public class HelloController {
 
     private final AlgorithmService algorithmService;
